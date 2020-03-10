af69563f717392dfa22cb257cfa392fa5e65e3fd
Sunshine1223 <2279915104@qq.com>
2020-03-10 15:09:08 +0800

    mentor-select初始化

---
+++ src/main/java/com/example/mentorselect/MentorSelectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentorselect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorSelectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorSelectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mentorselect/MentorSelectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentorselect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorSelectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
